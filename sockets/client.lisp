bfd8b24d67e52afa8150d086ca33bd36e0c9938e
spainisnotequal <spainisnotequal@gmail.com>
2020-11-25 12:12:25 +0100

    Run a function on the server with the numeric data sent by the client

--- sockets/client.lisp
+++ sockets/client.lisp
@@ -5,8 +5,9 @@
 (defun run-client (port)
   (usocket:with-client-socket (socket stream "localhost" port)
     ;; Send a message to the server
-    (format stream "Hello from the client!")
-    (finish-output stream)
+    (let ((number 1.5))
+      (format stream "~d" number)
+      (finish-output stream))
 
     ;; Receive a message to the server
     (format t "Data received: ~a~%" (read-line stream))))
--- sockets/server.py
+++ sockets/server.py
@@ -7,14 +7,28 @@ PORT = 54321        # Port to listen on (non-privileged ports are > 1023)
 ADDRESS_FAMILY = socket.AF_INET
 SOCKET_TYPE = socket.SOCK_STREAM
 
-with socket.socket(ADDRESS_FAMILY, SOCKET_TYPE) as s:
-    s.bind(("localhost", PORT))
-    s.listen()
-    print("Server listening...")
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by:", addr)
-        data = conn.recv(1024).decode()
-        print("Data received:", data)
-        message = "Bye from the server!"
-        conn.sendall(message.encode())
+def add1(n):
+    return n + 1
+
+def parse_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
+
+def run_server(port):
+    with socket.socket(ADDRESS_FAMILY, SOCKET_TYPE) as s:
+        s.bind(("localhost", port))
+        s.listen()
+        print("Server listening...")
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by:", addr)
+            data = conn.recv(1024).decode()
+            data = parse_number(data) # convert the string to a number to process de data
+            print("Data received:", data)
+            response = add1(data)
+            response = str(response) # convert the number to a string to send it to the client
+            conn.sendall(response.encode())
+
+run_server(PORT)
