a6849138e76e2b58366be73e5e573c1808f916f1
spainisnotequal <spainisnotequal@gmail.com>
2020-11-25 17:32:07 +0100

    Add a function that uses a function that imports functions from other modules

--- Py4CL2/my-own-module.lisp
+++ Py4CL2/my-own-module.lisp
@@ -2,11 +2,9 @@
 
 ;; The Python module should be located in $HOME/.local/lib/python3.7/site-packages
 ;; The following expression loads the module "my_own_module.py":
-
 (py4cl2:defpymodule "my_own_module" nil :lisp-package "MOM")
 
 ;; To get the symbols (the names of the functions) imported in the package, use the following function:
-
 (defun get-all-symbols (&optional package)
   (let ((lst ())
         (package (find-package package)))
@@ -23,3 +21,6 @@
 
 ;; Now, we can use the Python function add1(n):
 (mom:add-1 :n 1)
+
+;; And the Python function os_version():
+(mom:os-version)
