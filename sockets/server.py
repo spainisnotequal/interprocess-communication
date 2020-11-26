ef7b346b8ee9ca7f521d1e9bbce98c5fbe7029ea
spainisnotequal <spainisnotequal@gmail.com>
2020-11-26 08:57:51 +0100

    Autoformat the code using autopep8

--- sockets/server.py
+++ sockets/server.py
@@ -7,14 +7,17 @@ PORT = 54321        # Port to listen on (non-privileged ports are > 1023)
 ADDRESS_FAMILY = socket.AF_INET
 SOCKET_TYPE = socket.SOCK_STREAM
 
+
 def add1(n):
     return n + 1
+
 
 def parse_number(s):
     try:
         return int(s)
     except ValueError:
         return float(s)
+
 
 def run_server(port):
     with socket.socket(ADDRESS_FAMILY, SOCKET_TYPE) as s:
@@ -25,10 +28,13 @@ def run_server(port):
         with conn:
             print("Connected by:", addr)
             data = conn.recv(1024).decode()
-            data = parse_number(data) # convert the string to a number to process de data
+            # convert the string to a number to process de data
+            data = parse_number(data)
             print("Data received:", data)
             response = add1(data)
-            response = str(response) # convert the number to a string to send it to the client
+            # convert the number to a string to send it to the client
+            response = str(response)
             conn.sendall(response.encode())
+
 
 run_server(PORT)
